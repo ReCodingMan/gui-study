b593c0d8194b001ce88d89faf6b669e2c910453f
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-14 17:40:04 +0800

    布局测试

---
+++ project/src/com/cc/lesson01/ExDemo.java
@@ -0,0 +1,42 @@
+package com.cc.lesson01;
+
+import java.awt.*;
+
+public class ExDemo {
+
+    public static void main(String[] args) {
+
+        //总 Frame
+        Frame frame = new Frame();
+        frame.setSize(400,400);
+        frame.setLocation(300,400);
+        frame.setBackground(Color.BLACK);
+        frame.setVisible(true);
+        frame.setLayout(new GridLayout(2,1));
+
+        //4个面板
+        Panel panel = new Panel(new BorderLayout());
+        Panel pane2 = new Panel(new GridLayout(2,1));
+        Panel pane3 = new Panel(new BorderLayout());
+        Panel pane4 = new Panel(new GridLayout(2,2));
+
+        //上面搞定
+        panel.add(new Button("East-1"), BorderLayout.EAST);
+        panel.add(new Button("West-1"), BorderLayout.WEST);
+        pane2.add(new Button("p2-btn-1"));
+        pane2.add(new Button("p2-btn-2"));
+        panel.add(pane2, BorderLayout.CENTER);
+
+        //下面
+        pane3.add(new Button("East-1"), BorderLayout.EAST);
+        pane3.add(new Button("West-1"), BorderLayout.WEST);
+        //中间4个
+        for (int i=0; i<4; i++) {
+            pane4.add(new Button("for" + i));
+        }
+        pane3.add(pane4, BorderLayout.CENTER);
+
+        frame.add(panel);
+        frame.add(pane3);
+    }
+}
